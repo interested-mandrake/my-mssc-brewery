96da3fe776db4dae680fbd0087292ebff8092e23
interested-mandrake <ssepanski343@gmail.com>
2020-12-15 08:03:55 -0600

    initial commit

---
+++ src/test/java/com/interestedmandrake/mymsscbrewery/MyMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interestedmandrake.mymsscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
